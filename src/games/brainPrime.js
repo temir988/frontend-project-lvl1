e45c13496b72d935ffa7f73d0c3f2f04c2af126e
Temirlan <bamblebee988@gmail.com>
2020-03-21 20:23:44 +0200

    refactor isPrime function

--- src/games/brainPrime.js
+++ src/games/brainPrime.js
@@ -8,7 +8,7 @@ const isPrime = (num) => {
     return false;
   }
 
-  const halfNum = Math.round(num / 2);
+  const halfNum = num / 2;
   for (let i = 2; i <= halfNum; i += 1) {
     if (num % i === 0) {
       return false;
